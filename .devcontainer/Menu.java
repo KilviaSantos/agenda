f9a61c9cf7b99dfef298be12cac10b77a9330e0e
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2023-09-20 01:01:00 +0000

    :card_file_box: Create file Menu

---
+++ .devcontainer/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+
+    private void printMenu() {
+        out.println("Hello, " + System.getProperty("user.name") + "!");
+        out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
+        out.println("|                                  Bem-vindo ao Menu da sua Agenda, escolha entre as opções:                                                   |");
+        out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
+        out.println("| 0. Sair                    | 1. Incluir Contato         | 2. Consultar Contato        | 3. Alterar Contato           | 4. Deletar Contato    |");
+        out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
+      
+    
+    }
+}
