30c94778afad71f68675b78826ce6563fc5e5b4a
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2023-09-20 01:36:49 +0000

    :recycle: Melhoria do Menu

--- .devcontainer/Menu.java
+++ .devcontainer/Menu.java
@@ -1,12 +1,14 @@
 public class Menu {
-
-    private void printMenu() {
-        out.println("Olá, querido usuário !");
-        out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
-        out.println("|                                  Bem-vindo ao Menu da sua Agenda, escolha entre as opções:                                                   |");
-        out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
-        out.println("| 0. Sair                    | 1. Incluir Contato         | 2. Consultar Contato        | 3. Alterar Contato           | 4. Deletar Contato    |");
-        out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
+public static void main(String[] args) {
+    printMenu();
+}
+    private static void printMenu() {
+        System.out.println("|                                              Olá, querido usuário !                                                                          |");
+        System.out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
+        System.out.println("|                                  Bem-vindo ao Menu da sua Agenda, escolha entre as opções:                                                   |");
+        System.out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
+        System.out.println("| 0. Sair                    | 1. Incluir Contato         | 2. Consultar Contato        | 3. Alterar Contato           | 4. Deletar Contato    |");
+        System.out.println("+----------------------------------------------------------------------------------------------------------------------------------------------+");
       
     
     }
