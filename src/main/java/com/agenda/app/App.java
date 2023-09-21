b65f6f2b3f79a9fa9b7f2737a5ec465b4e696973
AbraaoAlves <abraao.teodosio@gmail.com>
2023-09-21 01:36:24 +0000

    test: connection with local postgres docker service

--- src/main/java/com/agenda/app/App.java
+++ src/main/java/com/agenda/app/App.java
@@ -1,5 +1,8 @@
 package com.agenda.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+ 
 /**
  * App
  */
@@ -7,5 +10,30 @@ public class App {
 
   public static void main(String[] args) {
     System.out.println("Hello world");
+    connect_to_db("postgres", "postgres", "postgres");
   }
+
+   public static Connection connect_to_db(String dbname, String user, String pass)
+    {
+        Connection con_obj=null;
+        String url="jdbc:postgresql://localhost:5432/";
+ 
+        try
+        {
+            con_obj= DriverManager.getConnection(url+dbname,user,pass);
+            if(con_obj!=null)
+            {
+                System.out.println("Connection established successfully !");
+            }
+            else
+            {
+                System.out.println("Connection failed !!");
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return con_obj;
+    }
 }
